30654cc61052b855d86ab6bd6fb728e7c2c0206c
hongsebin <hongsb5837@gmail.com>
2023-04-21 00:02:01 +0900

    Firebase 데이터 연결, 추가중
    Firebase 데이터 연결, 추가중
    
    context > AuthContext.js 추가
    context > ChatContext.js 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useContext } from "react";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import "./style.scss"
+import {
+  BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 function App() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  const ProtectRoute = ({children}) => {
+    if(!currentUser) {
+      return <Navigate to="/login" />
+    }
+
+    return children
+  }
+
   return (
-    <Register/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={
+            <ProtectRoute>
+              <Home />
+            </ProtectRoute>
+          } />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
